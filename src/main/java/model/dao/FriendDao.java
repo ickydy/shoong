7e1f3864e9caf370918fd32479620a357ef72225
tomcscscs <bright.chan88@gmail.com>
2023-12-06 15:15:23 +0900

    더미를 만들었는데 좀 이상한 듯
    더미를 만들었는데 약간 이상한거 같아 좀 함부로 실행하기가 그렇네 테이블 데이터에 영향을 줄까봐. 한번 봐줄래?

--- src/main/java/model/dao/FriendDao.java
+++ src/main/java/model/dao/FriendDao.java
@@ -35,6 +35,37 @@ public class FriendDao {
 		}
 		return result;
 	}
+	
+	
+	public boolean dummyFriend(Friend one) throws ClassNotFoundException { // 친구추가
+		boolean result = false;
+		// 1. 데이터 베이스 연결
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@13.125.229.23:1521:xe", "shoong",
+				"oracle")) {
+			// 2. 필요한 작업요청을 전송하고 응답을 받으면 됨.
+			String sql = "INSERT INTO friends VALUES(friend_seq, ?, ?, 0, null, 0)";
+			
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, "user_id");
+			pst.setString(2, "friend_id");
+			pst.setInt(3, 0);
+			pst.setDate(4,null);
+			pst.setInt(5, 0);
+			
+			
+			
+			
+			
+			int n = pst.executeUpdate(); // 요청 전송하고 DB에서 응답을 받아옴.
+			if (n == 1) {
+				result = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 	public List<Friend> findById(String userId) throws ClassNotFoundException { // 친구 검색.
 
