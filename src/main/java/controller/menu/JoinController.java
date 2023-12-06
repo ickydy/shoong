71adfce205f54b20675757b63600bbc7810d7e2f
JooSooYoung <150990783+JooSooYoung@users.noreply.github.com>
2023-12-06 16:23:45 +0900

    2023 12 06
    abcd

--- src/main/java/controller/menu/JoinController.java
+++ src/main/java/controller/menu/JoinController.java
@@ -1,21 +1,44 @@
 package controller.menu;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dao.AvatarDao;
+import model.dao.CountryDao;
+import model.dao.UserDao;
+import model.vo.Avatar;
+import model.vo.Country;
+import model.vo.User;
 
 @WebServlet("/join")
 public class JoinController extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		// 아바타 리스트
+		AvatarDao avatarDao = new AvatarDao();
+		try {
+			List<Avatar> avatars = avatarDao.findAll();
+			request.setAttribute("avatars", avatars);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// 국가 리스트
+		CountryDao countryDao = new CountryDao();
+		try {
+			List<Country> countries = countryDao.findAll();
+			request.setAttribute("countries", countries);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		request.getRequestDispatcher("/WEB-INF/view/join.jsp").forward(request, response);
 	}
 
@@ -23,6 +46,53 @@ public class JoinController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.sendRedirect(request.getContextPath() + "/index");
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String birth = request.getParameter("birth");
+		String name = request.getParameter("name");
+		String countryId = request.getParameter("countryId");
+		String gender = request.getParameter("gender");
+		String openAccess = request.getParameter("open_access");
+		String avatarsId = request.getParameter("avatars_id");
+
+		try {
+
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			if (id == null || id.equals("") || password == null || password.equals("") || birth == null
+					|| birth.equals("") || name == null || name.equals("") || countryId == null || countryId.equals("")
+					|| gender == null || gender.equals("") || openAccess == null || openAccess.equals("")
+					|| avatarsId == null || avatarsId.equals("")) {
+				request.setAttribute("error", "모든 것을 입력해주십시오.");
+				request.getRequestDispatcher("/WEB-INF/view/join.jsp").forward(request, response);
+				return;
+			}
+			int openAccessInt = Integer.parseInt(openAccess);
+			int avatarsIdInt = Integer.parseInt(avatarsId);
+
+			Date birthDate = new Date(sdf.parse(request.getParameter("birth")).getTime());
+			User user = new User(id, password, birthDate, name, countryId, gender, openAccessInt, avatarsIdInt);
+
+			UserDao userDao = new UserDao();
+			User found = userDao.findById(id);
+			if (found == null) {
+				boolean result = userDao.save(user);
+				request.setAttribute("result", result);
+				request.getRequestDispatcher("/WEB-INF/view/join.jsp").forward(request, response);
+			} else {
+				String e = "이미 존재 하는 계정입니다.";
+				request.setAttribute("e", e);
+				request.setAttribute("tempUser", user);
+
+				AvatarDao avatarDao = new AvatarDao();
+				List<Avatar> avatars = avatarDao.findAll();
+				CountryDao countryDao = new CountryDao();
+				List<Country> countries = countryDao.findAll();
+				request.setAttribute("avatars", avatars);
+				request.setAttribute("countries", countries);
+				request.getRequestDispatcher("/WEB-INF/view/join.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/controller/menu/LoginController.java
+++ src/main/java/controller/menu/LoginController.java
@@ -1,12 +1,17 @@
 package controller.menu;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.util.UUID;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dao.UserDao;
+import model.vo.User;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
@@ -20,7 +25,42 @@ public class LoginController extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.sendRedirect(request.getContextPath() + "/index");
-	}
+		String loginId = request.getParameter("loginId");
+        String loginPassword = request.getParameter("loginPassword");
+        String keep = request.getParameter("keep"); // 로그인 유지
+
+        try {
+            UserDao userDao = new UserDao();
+            User foundUser = userDao.findById(loginId);
+            if (foundUser != null && foundUser.getPassword().equals(loginPassword)) {
+                request.getSession().setAttribute("logonUser", foundUser);
+
+                if ("on".equals(keep)) {
+                    String id = UUID.randomUUID().toString();
+                    Date expiredAt = new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 30);
+                    KeepTicket ticket = new KeepTicket(id, loginId, expiredAt);
+
+                    KeepTicketDao keepTicketDao = new KeepTicketDao();
+                    keepTicketDao.save(ticket);
+
+                    Cookie cookie = new Cookie("ticketCode", id);
+                    cookie.setPath(request.getServletContext().getContextPath());
+                    cookie.setMaxAge(60 * 60 * 24 * 30);
+                    response.addCookie(cookie);
+                }
+
+                response.sendRedirect(request.getServletContext().getContextPath() + "/index");
+            } else {
+                String errorMessage = "잘못된 아이디 또는 비밀번호입니다.";
+                request.setAttribute("error", errorMessage);
+                request.getRequestDispatcher("/WEB-INF/view/user/login_form.jsp").forward(request, response);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            String errorMessage = "로그인 처리 중 오류가 발생했습니다.";
+            request.setAttribute("error", errorMessage);
+            request.getRequestDispatcher("/WEB-INF/view/user/login_form.jsp").forward(request, response);
+        }
+    }
 }
