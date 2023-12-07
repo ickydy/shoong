934254baefe352db3e0538d736af8bcf108d2a90
tomcscscs <bright.chan88@gmail.com>
2023-12-07 18:54:57 +0900

    스벅에서 컨트롤러(2)
    addFriend 컨트롤러 doGet에서 doPost로 수정했습니다.

--- src/main/java/controller/secret/AddFriendController.java
+++ src/main/java/controller/secret/AddFriendController.java
@@ -11,20 +11,18 @@ import model.dao.FriendDao;
 import model.vo.User;
 
 @WebServlet("/private/friends/add")
-public class AddFriendController extends HttpServlet {
-
+public class AddFriendController extends HttpServlet { // 여기 doget 에서 dopost로 변경.
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		User user =(User)request.getSession().getAttribute("logonUser");// 세션을 뽑아두고 여기서 get userId를 할거야.
-		
+
+		User user = (User) request.getSession().getAttribute("logonUser");// 세션을 뽑아두고 여기서 get userId를 할거야.
 
 		String userId = user.getId();
 		String friendId = request.getParameter("friendId");
 
 		FriendDao friendDao = new FriendDao();
-	
+
 		try {
 			boolean addFriendResult = friendDao.addFriend(userId, friendId);
 			request.setAttribute("addFriendResult", addFriendResult);// result 추가.
@@ -36,4 +34,5 @@ public class AddFriendController extends HttpServlet {
 		response.sendRedirect(request.getContextPath() + "/private/friends");
 
 	}
+
 }
